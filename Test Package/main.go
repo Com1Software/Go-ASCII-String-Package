e634f2c774ea3dbeb58583a3a85deaf7399ab014
Lenovo 15 <information@com1software.com>
2024-06-22 15:59:04 -0400

    update

--- Test Package/main.go
+++ Test Package/main.go
@@ -3,39 +3,24 @@ package main
 import (
 	"fmt"
 	"runtime"
-	"strings"
+        
+        "github.com/Com1Software/Go-ASCII-String-Package"
+	
 )
 
 func main() {
+	
 	fmt.Printf("(c) Copyright Com1Software 1992-2024\n")
 	fmt.Printf("Operating System : %s\n", runtime.GOOS)
 	fmt.Println(string(34))
 	for x := 1; x < 128; x++ {
-		fmt.Printf(ASCIIToString(x))
+		fmt.Printf(asciistring.ASCIIToString(x))
 	}
 	fmt.Println("")
 	chr := "A"
-	fmt.Println(StringToASCII(chr))
+	fmt.Println(asciistring.StringToASCII(chr))
 	chr = "B"
-	fmt.Println(StringToASCII(chr))
+	fmt.Println(asciistring.StringToASCII(chr))
 	chr = "C"
-	fmt.Println(StringToASCII(chr))
-}
-
-func StringToASCII(val string) int {
-	chr := ""
-	rtn := 0
-	for x := 1; x < 128; x++ {
-		chr = ASCIIToString(x)
-		if strings.Contains(chr, val) {
-			rtn = x
-			x = 128
-		}
-	}
-	return rtn
-}
-
-func ASCIIToString(val int) string {
-	rtn := string(val)
-	return rtn
+	fmt.Println(asciistring.StringToASCII(chr))
 }
