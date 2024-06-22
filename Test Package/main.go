7e44421aee71944fa7518f6f57900c98ed6a630c
Lenovo 15 <information@com1software.com>
2024-06-22 16:03:23 -0400

    Update

--- Test Package/main.go
+++ Test Package/main.go
@@ -11,7 +11,6 @@ func main() {
 
 	fmt.Printf("(c) Copyright Com1Software 1992-2024\n")
 	fmt.Printf("Operating System : %s\n", runtime.GOOS)
-	fmt.Println(string(34))
 	for x := 1; x < 128; x++ {
 		fmt.Printf(asciistring.ASCIIToString(x))
 	}
